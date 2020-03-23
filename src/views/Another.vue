adad46909021dd5b1e72eb701392b82bf86c20ba
21diego <morinigodiego21@gmail.com>
2020-03-23 15:28:48 -0300

    blank

---
+++ src/views/Another.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="about">
+    <h1>This is another</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'About'
+  }
+
+</script>
+
+<style scoped>
+
+</style>
